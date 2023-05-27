7711d1298c465500a403d61f7f14f5541bca401d
JayBansod <86622169+JayBansod@users.noreply.github.com>
2023-05-27 09:00:54 +0530

    a

---
+++ prims.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+#define V 5
+
+int minKey(int key[], bool visited[])
+{
+
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (visited[v] == false && key[v] < min)
+            min = key[v],
+            min_index = v;
+
+    return min_index;
+}
+
+void printMST(int parent[], int graph[V][V])
+{
+    cout << "Edge \tWeight\n";
+    for (int i = 1; i < V; i++)
+        cout << parent[i] << " - " << i << " \t"
+             << graph[i][parent[i]] << " \n";
+}
+
+void primMST(int graph[V][V])
+{
+    int parent[V]; // store constructed MST answer
+
+    int key[V]; // to get minimum weight
+
+    bool visited[V]; // visited vertices
+
+    for (int i = 0; i < V; i++)
+        key[i] = INT_MAX, // set distance as infinite
+            visited[i] = false;
+
+    key[0] = 0; // to go 0 to 0 => 0 required
+
+    parent[0] = -1; // First node is always root of MST
+
+    for (int count = 0; count < V - 1; count++)
+    {
+
+        int u = minKey(key, visited);
+
+        visited[u] = true; // set as visited
+
+        // Update key value and parent index of
+        // the adjacent vertices of the picked vertex.
+        // Consider only those vertices which are not
+        // yet included in MST
+        for (int v = 0; v < V; v++)
+
+            // graph[u][v] is non zero only for adjacent
+            // vertices of m visited[v] is false for vertices
+            // not yet included in MST Update the key only
+            // if graph[u][v] is smaller than key[v]
+            if (graph[u][v] && visited[v] == false && graph[u][v] < key[v])
+                parent[v] = u, key[v] = graph[u][v];
+    }
+
+    printMST(parent, graph);
+}
+
+int main()
+{
+    int graph[V][V] = {{0, 2, 0, 6, 0},
+                       {2, 0, 3, 8, 5},
+                       {0, 3, 0, 0, 7},
+                       {6, 8, 0, 0, 9},
+                       {0, 5, 7, 9, 0}};
+
+    primMST(graph);
+
+    return 0;
+}
